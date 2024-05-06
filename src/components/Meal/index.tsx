2d70fb2fcbc6a797aeab74a2c0e9c0423e038aa1
Simon Sinclair <simon@simonsinclair.com>
2024-05-06 18:14:08 +0100

    Create components/Meal draggable

---
+++ src/components/Meal/index.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useRef } from 'react';
+import { action } from 'mobx';
+import { observer, useLocalObservable } from 'mobx-react-lite';
+import { draggable } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
+
+import type { Meal as MealModel } from '../../models/meal';
+import clsx from 'clsx';
+
+interface Properties {
+  meal: MealModel;
+}
+
+export const Meal = observer(({ meal }: Properties) => {
+  const elementRef = useRef<HTMLDivElement | null>(null);
+  const state = useLocalObservable(() => ({
+    isDragging: false,
+  }));
+
+  useEffect(() => {
+    const element = elementRef.current;
+    if (element === null) {
+      throw new TypeError('Meal element reference is `null`');
+    }
+
+    return draggable({
+      element,
+      onDragStart: action(() => {
+        state.isDragging = true;
+      }),
+      onDrop: action(() => {
+        state.isDragging = false;
+      }),
+    });
+  }, [state]);
+
+  return (
+    <div
+      ref={elementRef}
+      key={meal.id}
+      className={clsx(
+        'space-y-1 rounded bg-white p-2 shadow hover:text-blue-500',
+        {
+          'opacity-50': state.isDragging,
+          'cursor-grab': !state.isDragging,
+          'cursor-grabbing': state.isDragging,
+        },
+      )}
+    >
+      <div className="text-sm font-medium">{meal.name}</div>
+      <ul className="flex gap-1 text-xs tracking-wide">
+        <li className="rounded bg-slate-100 px-1.5 py-0.5 font-mono">
+          {meal.energy}kcal
+        </li>
+        <li className="rounded bg-slate-100 px-1.5 py-0.5 font-mono">
+          {meal.protein}g
+        </li>
+        <li className="rounded bg-slate-100 px-1.5 py-0.5">{meal.type}</li>
+      </ul>
+    </div>
+  );
+});
--- src/components/MealList/index.tsx
+++ src/components/MealList/index.tsx
@@ -1,10 +1,11 @@
 import { observer } from 'mobx-react-lite';
 
-import { Meal } from '../../models/meal';
+import { Meal as MealModel } from '../../models/meal';
 import { MealType } from '../../types';
+import { Meal } from '../Meal';
 
 interface Properties {
-  meals: Meal[];
+  meals: MealModel[];
   type?: '' | MealType;
 }
 
@@ -19,27 +20,9 @@ export const MealList = observer(({ meals, type = '' }: Properties) => {
             return meal.type === type;
           }
         })
-        .map((meal) => {
-          return (
-            <li
-              key={meal.id}
-              className="space-y-1 rounded bg-white p-2 shadow hover:shadow-blue-600/25"
-            >
-              <div className="text-sm font-medium">{meal.name}</div>
-              <ul className="flex gap-1 text-xs tracking-wide">
-                <li className="rounded bg-slate-100 px-1.5 py-0.5 font-mono">
-                  {meal.energy}kcal
-                </li>
-                <li className="rounded bg-slate-100 px-1.5 py-0.5 font-mono">
-                  {meal.protein}g
-                </li>
-                <li className="rounded bg-slate-100 px-1.5 py-0.5">
-                  {meal.type}
-                </li>
-              </ul>
-            </li>
-          );
-        })}
+        .map((meal) => (
+          <Meal key={meal.id} meal={meal} />
+        ))}
     </ul>
   );
 });
