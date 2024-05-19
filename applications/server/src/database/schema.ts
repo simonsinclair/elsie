d9971fbfc74711d0b6762952d8c02a9248943dd8
Simon Sinclair <simon@simonsinclair.com>
2024-05-19 17:32:26 +0100

    Update schema.ts

--- applications/server/src/database/schema.ts
+++ applications/server/src/database/schema.ts
@@ -1,9 +1,21 @@
-import { pgTable, uuid } from 'drizzle-orm/pg-core';
+import { numeric, pgTable, text, uuid } from 'drizzle-orm/pg-core';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
+
+export const mealPlans = pgTable('meal_plans', {
+  id: uuid('id').primaryKey(),
+  name: text('name'),
+});
 
 export const meals = pgTable('meals', {
   id: uuid('id').primaryKey(),
+  name: text('name').notNull(),
+  energy: numeric('energy').notNull(),
+  protein: numeric('protein').notNull(),
+  mealPlanId: uuid('meal_plan_id').references(() => mealPlans.id),
 });
 
 export const insertMealSchema = createInsertSchema(meals);
 export const selectMealSchema = createSelectSchema(meals);
+
+export const insertMealPlanSchema = createInsertSchema(mealPlans);
+export const selectMealPlanSchema = createSelectSchema(mealPlans);
