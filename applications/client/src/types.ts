67ca6650e57807a5e741527986e448d8ba098ab9
Simon Sinclair <simon@simonsinclair.com>
2024-06-23 19:48:28 +0100

    Send new actions to websocket clients

--- applications/client/src/types.ts
+++ applications/client/src/types.ts
@@ -6,7 +6,11 @@ export type MealType = 'BREAKFAST' | 'LUNCH' | 'DINNER' | 'SNACK' | 'DRINK';
 /** WebSocket server messages */
 export const serverMessageSchema = z
   .object({
-    type: z.union([z.literal('initialise'), z.literal('pong')]),
+    type: z.union([
+      z.literal('initialise'),
+      z.literal('pong'),
+      z.literal('actions'),
+    ]),
   })
   .passthrough();
 export type WebSocketMessage = z.infer<typeof serverMessageSchema>;
--- applications/server/src/websocket.ts
+++ applications/server/src/websocket.ts
@@ -1,6 +1,8 @@
 import WebSocket from 'ws';
+import { desc, gt } from 'drizzle-orm';
 
-import { client } from './database';
+import { database } from './database';
+import { actions } from './database/schema';
 
 const PORT = 4000;
 
@@ -14,14 +16,56 @@ const server = new WebSocket.WebSocketServer({
  * clients issues pings, plus a conservative assumption of the latency.
  */
 const TIMEOUT_MS = 32_000;
+const POLL_TIMEOUT_MS = 2_500;
+
+let latestActionId: number | null = null;
+let pollTimeout: ReturnType<typeof setTimeout>;
+
+const poll = async () => {
+  try {
+    const result =
+      latestActionId === null
+        ? await database.select().from(actions).orderBy(actions.id)
+        : await database
+            .select()
+            .from(actions)
+            .where(gt(actions.id, latestActionId))
+            .orderBy(actions.id);
+
+    if (result.length > 0) {
+      for (const client of server.clients) {
+        if (client.OPEN) {
+          client.send(JSON.stringify({ type: 'actions', actions: result }));
+        }
+      }
+
+      latestActionId = result[result.length - 1].id;
+    }
+  } catch (error) {
+    console.error('poll_error', error);
+  } finally {
+    pollTimeout = setTimeout(poll, POLL_TIMEOUT_MS);
+  }
+};
 
 server.on('listening', async () => {
-  await client.listen('table_actions_insert', (value) => {
-    console.log('notification', value);
+  try {
+    const result = await database
+      .select({ id: actions.id })
+      .from(actions)
+      .orderBy(desc(actions.id))
+      .limit(1);
+    if (result.length === 1) latestActionId = result[0].id;
+
     /**
-     * @todo Send `value` to all connected websocket clients.
+     * @todo
+     * We only poll() when fetching the latest action succeeds. If it fails,
+     * we'll never send actions - we should probably implement a retry.
      */
-  });
+    poll();
+  } catch (error) {
+    console.error('latest_action_id_error', error);
+  }
 });
 
 server.on('connection', (websocket) => {
@@ -37,7 +81,7 @@ server.on('connection', (websocket) => {
       const data = JSON.parse(message.toString());
 
       if (data.type === 'initialise') {
-        websocket.send(JSON.stringify({ type: 'initialise' }));
+        websocket.send(JSON.stringify({ type: 'initialise', latestActionId }));
       }
 
       if (data.type === 'ping') {
@@ -65,5 +109,6 @@ server.on('connection', (websocket) => {
 });
 
 server.on('close', () => {
+  clearTimeout(pollTimeout);
   console.log('websocket server: closed');
 });
